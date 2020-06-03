fc0eb31546e1293c7173d5f8fc0fc17333fcefd2
Farnaz Ardenti <farnazardenti@gmail.com>
2020-06-03 17:12:03 +0100

    changed port

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
   module: {
     rules: [
       {
